2e01e9ab780a8d4fe154c2fb06eee6685749c7be
danisluis6 <vuongluis@hotmail.com>
2016-12-30 16:51:19 +0700

    ok

--- README.md
+++ README.md
@@ -23,4 +23,18 @@ Technical Android Json
     * STEP THREE: Let's start practise with json and forget about knowledge.
 #why ? => After practise and you remember deep about it.
 
+    *** BREAK STEP: 
+     - Provide permission to connect internet
+
+     - Think about: Using AsyncTask get Object InputStream(From Internet) and compare StringBuilder
+     ? Using StringBuilder without String: Every line contain sentence 
+     Example: I love you\nso much <=> StringBuilder <=> I love you (one line) and so much(one line)
+     Solve: Writting Class HttpHandler.java
+
+     - Break custom ListView and Declare Toolbar.
+
+<p align="center">
+  <img src="https://github.com/danisluis6/Technical-Android-Json/blob/version1/version1/3.png">
+</p>
+
 
--- version1/app/src/main/java/com/example/enclaveit/json/MainActivity.java
+++ version1/app/src/main/java/com/example/enclaveit/json/MainActivity.java
@@ -1,13 +1,56 @@
 package com.example.enclaveit.json;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.View;
+import android.view.Window;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private String TAG = MainActivity.class.getSimpleName();
+    private ProgressDialog pDialog;
+    private ListView listView;
+
+    private String URL = "http://api.androidhive.info/contacts/";
+    private ArrayList<HashMap<String, String>> contactList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar)this.findViewById(R.id.toolbar);
+        toolbar.setNavigationIcon(R.mipmap.ic_launcher);
+        this.setSupportActionBar(toolbar);
+
+        contactList = new ArrayList<>();
+
+        addOnListener();
+    }
+
+    private void addOnListener() {
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"Ok",Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        /** Copy menu of Popup Menu and Insert **/
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
     }
 }
