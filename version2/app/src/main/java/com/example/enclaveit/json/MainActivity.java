1e46b65d788d0a6d6b75cfbfe935b9060a3e9300
danisluis6 <vuongluis@hotmail.com>
2016-12-30 17:10:59 +0700

    ok

--- version2/app/src/main/java/com/example/enclaveit/json/MainActivity.java
+++ version2/app/src/main/java/com/example/enclaveit/json/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.enclaveit.json;
 
 import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.Window;
+import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.Toast;
+
+import com.example.enclaveit.json.services.HttpJsonHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -52,4 +62,124 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.toolbar_menu, menu);
         return true;
     }
+
+    /**
+     * @author: Lorence
+     * Using AsyncTask to get json from Website.
+     * - If you using variable global to get json => you don't need parameter to get data. <Void,Void,Void>
+     * - If you don't variable global => you must get data follow Void1[Parameter to transport] and Void3[Result return]
+     */
+    private class GetContacts extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            // Showing progress dialog
+            pDialog = new ProgressDialog(MainActivity.this);
+            pDialog.setMessage("Please wait...");
+            pDialog.setCancelable(false);
+            pDialog.show();
+
+        }
+
+        @Override
+        protected Void doInBackground(Void... arg0) {
+            /**
+             * Replace using new Constructor(Parameter1,Parameter2,...)
+             * => Definition Object in that Class and get data by object to use
+             */
+            HttpJsonHandler sh = new HttpJsonHandler();
+
+            // Making a request to url and getting response
+            String jsonStr = sh.makeServiceCall(URL);
+            Log.e(TAG, "Response from url: " + jsonStr);
+            if (jsonStr != null) {
+                try {
+                    /**
+                     * Definite object to get Json by use JsonObject
+                     * In this case: it 's contact
+                     */
+                    JSONObject jsonObj = new JSONObject(jsonStr);
+                    /**
+                     * Definite object to get Json by use JsonObject
+                     * In this case: it 's contacts
+                     */
+                    JSONArray contacts = jsonObj.getJSONArray("contacts");
+                    /**
+                     * Loop to get all data from json
+                     */
+                    for (int i = 0; i < contacts.length(); i++) {
+                        JSONObject c = contacts.getJSONObject(i);
+                        String id = c.getString("id");
+                        String name = c.getString("name");
+                        String email = c.getString("email");
+                        String address = c.getString("address");
+                        String gender = c.getString("gender");
+
+                        // Phone node is JSON Object
+                        JSONObject phone = c.getJSONObject("phone");
+                        String mobile = phone.getString("mobile");
+                        String home = phone.getString("home");
+                        String office = phone.getString("office");
+
+                        // tmp hash map for single contact
+                        HashMap<String, String> contact = new HashMap<>();
+                        // adding each child node to HashMap key => value
+                        contact.put("id", id);
+                        contact.put("name", name);
+                        contact.put("email", email);
+                        contact.put("mobile", mobile);
+                        // adding contact to contact list
+                        contactList.add(contact);
+                    }
+                } catch (final JSONException e) {
+                    Log.e(TAG, "Json parsing error: " + e.getMessage());
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getApplicationContext(),
+                                    "Json parsing error: " + e.getMessage(),
+                                    Toast.LENGTH_LONG)
+                                    .show();
+                        }
+                    });
+                }
+            } else {
+                Log.e(TAG, "Couldn't get json from server.");
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(),
+                                "Couldn't get json from server. Check LogCat for possible errors!",
+                                Toast.LENGTH_LONG)
+                                .show();
+                    }
+                });
+
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            if (pDialog.isShowing())
+                pDialog.dismiss();
+            /**
+             * Updating parsed JSON data into ListView
+             * */
+            ListAdapter adapter = new SimpleAdapter(
+                    MainActivity.this, contactList,
+                    R.layout.item_listview, new String[]{"name", "email",
+                    "mobile"}, new int[]{R.id.name,
+                    R.id.email, R.id.mobile});
+            listView.setAdapter(adapter);
+        }
+
+    }
+
+    /**
+     * READ MORE: http://www.androidhive.info/2012/01/android-json-parsing-tutorial/
+     * App Android Studio
+     */
 }
